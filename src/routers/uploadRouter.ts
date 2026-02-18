4fcd6a5187adc8176ed6f06c8c83e19c7d5e373b
vishvahkdigiverse-svg <kenilhkdigiverc@gmail.com>
2026-02-18 16:58:12 +0530

    add upload router for image upload, retrieval, and deletion endpoints.

--- src/routers/uploadRouter.ts
+++ src/routers/uploadRouter.ts
@@ -8,6 +8,6 @@ const router = Router();
 
 router.post("/", upload.array("files"), handleUploadError, uploadController.uploadImages);
 router.get("/", uploadController.getImages);
-router.delete("/", roleCheck([AccountType.USER, AccountType.ADMIN]), uploadController.deleteImage);
+router.delete("/delete", verifyToken, roleCheck([AccountType.USER, AccountType.ADMIN]), uploadController.deleteImage);
 
 export default router;
